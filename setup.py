0484a573fff424c40289f61dee03e4079f593df8
Lojze Žust <lojze.zust@gmail.com>
2024-05-27 14:00:27 +0200

    Bump version 🔼

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='junit-eval',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'yacs',
